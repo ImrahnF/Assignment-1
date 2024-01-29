79fc9d6e3dd984eb2f8a092bd1390f32dcaf938e
Potato <imrahnfaqiri21@gmail.com>
2024-01-29 18:01:31 -0500

    Milestone 2 - Game Intro
    Simply welcomes the user and displays a brief rundown of the game. Some things are imported such as the 2 roles and a special random role!

--- App.py
+++ App.py
@@ -8,3 +8,30 @@ Functions:
 - start_game(): Initiates the game and coordinates the overall flow.
 - print_message(message): Prints a message to the user.
 """
+# variables and imports
+import Game   # Import the Game module
+import Role1  # Import the Role1 module
+import Role2  # Import the Role2 module
+import Role3  # Import SPECIAL RANDOM ROLE module
+
+import random
+
+# functions
+def start_game():
+    # This is the very beginning. It will print the introduction by welcoming the user and starting the game.
+    print_introduction()
+
+def print_introduction():
+    print("""  
+            ▒▐█▒▐█▒▐█▀▀▒██░░░▒██░░░▒▐█▀▀█▌ 
+            ▒▐████▒▐█▀▀▒██░░░▒██░░░▒▐█▄▒█▌ 
+            ▒▐█▒▐█▒▐█▄▄▒██▄▄█▒██▄▄█▒▐██▄█▌
+            
+Welcome to the text adventure!
+Embark on a thrilling quest where you'll choose your role, each with unique attributes. 
+Navigate challenges, roll the dice, and shape your destiny. Will you emerge victorious or succumb to the twists of fate? 
+The journey awaits! Choose wisely, and may the odds be in your favor.
+          """)
+
+# begin
+start_game()
