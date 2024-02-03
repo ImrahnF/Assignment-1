b62999f6bc31fce5f53f80f3ab22cc4602807953
Potato <imrahnfaqiri21@gmail.com>
2024-02-03 15:51:01 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,9 @@
 import random
 
-intelligence = 2
 luck = 2
+# [2] luck means you start with a card + Ace (value = 11) and know the next card is.
+# [1] luck means you start with card + Ace (value = 11)
+# [0 to -2] is just regular blackjack
 
 ranks = {
     '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
@@ -19,7 +21,7 @@ def print_card(rank, suit):
     print(f"| {rank}  {suit} |")
     print("+------+")
 
-def generate_card():
+def generate__random_card(start):
     selected_rank = random.choice(list(ranks.keys()))
     selected_suit = random.choice(suits)
     rank_value = 0
@@ -46,8 +48,27 @@ def generate_card():
         #print(f"Rank value: {selected_rank}")
         rank_value = ranks[selected_rank]
     
-    print_card(selected_rank, selected_suit)
+    # print_card(selected_rank, selected_suit)
     return [selected_rank, selected_suit, rank_value]
     
-items = generate_card()
+items = generate__random_card(luck)
 print(f"Rank: {[items[0]]} || Suit: {items[1]} || Value: {items[2]}")
+
+def play_blackjack():
+    running = True
+    player_set = [{'A': 11}]
+    next_card = []
+
+    # if luck is 2, keep playerset + show next card
+    # if luck is 1, keep playerset
+    # if luck is 0 to -2, clear playerset + add random cards
+
+    '''
+    # Print the information of the first card
+    first_card = player_set[0]
+    for rank, value in first_card.items():
+        print(f"First Card: {rank} - Value: {value}")
+    '''
+
+
+play_blackjack()
